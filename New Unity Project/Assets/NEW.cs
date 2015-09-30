3209d771bb69b84f57f35fc189f3740e10c053ad
EVASIL200 <vasilev.emil@yahoo.com>
2015-09-30 10:43:38 +0100

    NEW

---
+++ New Unity Project/Assets/NEW.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NEW : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
